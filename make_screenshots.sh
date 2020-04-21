4258d79e873fd63999c72b9bd7fff315ed7041b1
David Lindecrantz <optiroc@gmail.com>
2020-04-21 22:22:49 +0200

    Database update

--- make_screenshots.sh
+++ make_screenshots.sh
@@ -3,5 +3,5 @@
 for f in $1/*.png; do
     [ -f "$f" ] || break
     echo "Converting $f"
-    ./ags_screenshot.py --crop 640x512 --scale 320x256 --resample 320x128 --colors 128 -i "$f" -o "$f".iff
+    ./ags_screenshot.py --crop 640x512 --scale 320x256 --resample 320x128 --colors 128 -i "$f" -o "$f".iff -p "$f".out.png
 done
