bad5c71687cc8642a0a51ef529162a17e76023dc
David Lindecrantz <optiroc@gmail.com>
2020-12-21 00:38:12 +0100

    Refactor make_vadjust

--- make_vadjust.py
+++ make_vadjust.py
@@ -34,63 +34,71 @@ modes = [
     (0x0223D5E4, "PAL SuperHiRes Laced", False, True)
 ]
 
+def make_vadjust(pal_5x, v_offset):
+    out = bytes()
+    if v_offset is None:
+        v_offset = 0
+
+    for mode in modes:
+        id = mode[0]
+        ntsc = bool(mode[2])
+        lace = bool(mode[3])
+
+        # get base values
+        if ntsc:
+            h1 = ntsc_h[0]
+            h2 = ntsc_h[1]
+            v1 = ntsc_v[0]
+            v2 = ntsc_v[1]
+        else:
+            h1 = pal_h[0]
+            h2 = pal_h[1]
+            if pal_5x:
+                v1 = pal_v5[0]
+                v2 = pal_v5[1]
+            else:
+                v1 = pal_v[0]
+                v2 = pal_v[1]
+        # interlace adjustment
+        if lace:
+            v2 -= 1
+        # second value negative
+        h2 = 4096 - h2
+        v2 = 4096 - v2
+        # adjust for offset
+        if v_offset != 0:
+            vofs = v_offset
+            if v1 + vofs < 0:
+                vofs = -v1
+            if v2 + vofs >= 4095:
+                vofs = v2 - 4095
+            v1 += vofs
+            v2 += vofs
+
+        out += struct.pack("<I", id)
+        out += struct.pack("<H", h2)
+        out += struct.pack("<H", h1)
+        out += struct.pack("<H", v2)
+        out += struct.pack("<H", v1)
+        out += struct.pack("<I", 0)
+
+    if len(out) < VADJUST_LEN:
+        out += bytes(VADJUST_LEN - len(out))
+    return out
+
+# -----------------------------------------------------------------------------
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--out", dest="out_vadjust", metavar="FILE", help="output file")
     parser.add_argument("--pal5x", dest="pal_5x", action="store_true", default=False, help="make PAL crops for 5x scaling")
-    parser.add_argument("--voffs", dest="v_offset", type=int, default=0, help="vertical offset")
+    parser.add_argument("--vofs", dest="v_offset", type=int, default=0, help="vertical offset")
 
     try:
         args = parser.parse_args()
-        out = bytes()
-
-        for mode in modes:
-            id = mode[0]
-            ntsc = bool(mode[2])
-            lace = bool(mode[3])
-
-            # get base values
-            if ntsc:
-                h1 = ntsc_h[0]
-                h2 = ntsc_h[1]
-                v1 = ntsc_v[0]
-                v2 = ntsc_v[1]
-            else:
-                h1 = pal_h[0]
-                h2 = pal_h[1]
-                if args.pal_5x:
-                    v1 = pal_v5[0]
-                    v2 = pal_v5[1]
-                else:
-                    v1 = pal_v[0]
-                    v2 = pal_v[1]
-            # interlace adjustment
-            if lace:
-                v2 -= 1
-            # second value negative
-            h2 = 4096 - h2
-            v2 = 4096 - v2
-            # adjust for offset
-            if args.v_offset != 0:
-                vofs = args.v_offset
-                if v1 + vofs < 0:
-                    vofs = -v1
-                if v2 + vofs >= 4095:
-                    vofs = v2 - 4095
-                v1 += vofs
-                v2 += vofs
-
-            out += struct.pack("<I", id)
-            out += struct.pack("<H", h2)
-            out += struct.pack("<H", h1)
-            out += struct.pack("<H", v2)
-            out += struct.pack("<H", v1)
-            out += struct.pack("<I", 0)
-
-        if len(out) < VADJUST_LEN:
-            out += bytes(VADJUST_LEN - len(out))
 
         if args.out_vadjust:
+            out = make_vadjust(args.pal_5x, args.v_offset)
             with open(args.out_vadjust, "wb") as f:
                 f.write(out)
         return 0
