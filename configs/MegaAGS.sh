73e801c57760e5686249150fe59cb27d3be8fda0
Alex Limi <105389+limi@users.noreply.github.com>
2023-05-05 23:18:19 -0300

    Revert "Make these fit in the menu"
    This reverts commit aabba7c217f04607167b5f4de6884085f7427a8e.

--- configs/MegaAGS.sh
+++ configs/MegaAGS.sh
@@ -1,5 +1,5 @@
 # Post build script
-# cp -R $AGSCONTENT/distro/* $AGSDEST/
-# mv $AGSDEST/MegaAGS-Extras.md $AGSDEST/MegaAGS-Extras.txt
-# mv $AGSDEST/MegaAGS-History.md $AGSDEST/MegaAGS-History.txt
-# mv $AGSDEST/MegaAGS-ReadMe.md $AGSDEST/MegaAGS-ReadMe.txt
+cp -R $AGSCONTENT/distro/* $AGSDEST/
+mv $AGSDEST/MegaAGS-Extras.md $AGSDEST/MegaAGS-Extras.txt
+mv $AGSDEST/MegaAGS-History.md $AGSDEST/MegaAGS-History.txt
+mv $AGSDEST/MegaAGS-ReadMe.md $AGSDEST/MegaAGS-ReadMe.txt
