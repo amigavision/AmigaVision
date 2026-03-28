89361131b1f9e00e628311a14186f6077fdc5b44
Alex Limi <alex@limi.net>
2026-03-28 16:53:21 +0700

    Additional files needed

--- configs/AmigaVision.sh
+++ configs/AmigaVision.sh
@@ -1,5 +1,6 @@
 # Post build script
-cp -R "$AGSCONTENT"/distro/* "$AGSDEST"/
-mv "$AGSDEST"/Extras.md "$AGSDEST"/Extras.txt
-mv "$AGSDEST"/History.md "$AGSDEST"/History.txt
+cp -R ./content/distro/* "$AGSDEST"/
+[ -d "$AGSCONTENT"/distro/games/Amiga ] && cp -R "$AGSCONTENT"/distro/games/Amiga/* "$AGSDEST"/games/Amiga/
+[ -f "$AGSDEST"/Extras.md ] && mv "$AGSDEST"/Extras.md "$AGSDEST"/Extras.txt
+[ -f "$AGSDEST"/History.md ] && mv "$AGSDEST"/History.md "$AGSDEST"/History.txt
 cp ./ReadMe.md "$AGSDEST"/ReadMe.txt
